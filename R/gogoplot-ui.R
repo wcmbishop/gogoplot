1ecb05e4e35c4a4d48100c4aff63ca319c3137a5
Will Bishop <wbishop@tesla.com>
2017-10-16 17:48:17 -0700

    * removing old code from gogoplot-ui.R

--- R/gogoplot-ui.R
+++ R/gogoplot-ui.R
@@ -7,15 +7,5 @@ gogoplot_ui <- function() {
       codeTabPanel,
       dataTabPanel
     )
-    # miniUI::miniButtonBlock(
-    # shiny::actionButton('btn_update', 'Update Plot',
-    #                     icon = shiny::icon('chart')),
-    # shiny::actionButton('btn_plot', 'Return Plot',
-    #                     icon = shiny::icon('chart')),
-    #   shiny::actionButton('btn_code', 'Return Code',
-    #                       icon = shiny::icon('keyboard-o')),
-    #   shiny::downloadButton('btn_save', 'Save Plot'),
-    #   border = 'top'
-    # )
   )
 }
